b8aec2fca22b29e51ac866e7e4d4f3bcc4228298
basON <wch.basinski@gmail.com>
2022-08-15 23:57:34 +0200

    Add basic profile page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,107 +1,38 @@
-import {
-  InputAdornment,
-  FormControl,
-  InputLabel,
-  TextField,
-  OutlinedInput,
-  IconButton,
-  Button,
-} from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import React, { useState, ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "@mui/material";
-function SignUp() {
+
+function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
-  const [submitDisabled, setSubmitDisabled] = useState(true);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    firstname: "",
-    lastname: "",
-    charityid: "",
-  });
-  const [error, setError] = useState(true);
-  const [emailError, setEmailError] = useState(true);
-  const [firstnameError, setFirstnameError] = useState(true);
-  const [lastnameError, setLastnameError] = useState(true);
-  const [charityidError, setCharityidError] = useState(true);
-  const [passwordError, setPasswordError] = useState(true);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState(false);
 
-  const { email, password, firstname, lastname, charityid } = formData;
+  const { email, password } = formData;
   const navigate = useNavigate();
-  const isSubmitDisabled = () => {
-    return (
-      emailError ||
-      firstnameError ||
-      lastnameError ||
-      charityidError ||
-      passwordError
-    );
-  };
-  const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmailError(true);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setError(false);
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
-    if (isValidEmail(e.target.value)) {
-      setEmailError(false);
-    }
-    setSubmitDisabled(isSubmitDisabled());
   };
-  const onFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setFirstnameError(true);
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-    if (e.target.value.length > 5) {
-      setFirstnameError(false);
-    }
-    setSubmitDisabled(isSubmitDisabled());
-  };
-  const onLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setLastnameError(true);
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-    if (e.target.value.length > 5) {
-      setLastnameError(false);
-    }
-    setSubmitDisabled(isSubmitDisabled());
-  };
-  const onCharityIdChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setCharityidError(true);
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-    if (e.target.value.length > 5 && Number.isInteger(Number(charityid))) {
-      setCharityidError(false);
-    }
-    setSubmitDisabled(isSubmitDisabled());
-  };
-  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPasswordError(true);
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-    if (e.target.value.length > 7) {
-      setPasswordError(false);
-    }
-    setSubmitDisabled(isSubmitDisabled());
-  };
-
   function isValidEmail(email: string) {
     return /\S+@\S+\.\S+/.test(email);
   }
-
-  const onSubmit = () => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (isValidEmail(email)) {
-      return navigate("/");
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        return navigate("/");
+      }
     }
     return setError(true);
   };
@@ -113,63 +44,40 @@ function SignUp() {
         </header>
         <form className="flex flex-col gap-3 mx-6" onSubmit={onSubmit}>
           <TextField
-            error={emailError}
+            error={error}
             value={email}
             id="email"
             label="Email"
             variant="outlined"
-            onChange={onEmailChange}
+            onChange={onChange}
           />
-          {emailError && <p className="text-red-600">Invalid input.</p>}
 
           <TextField
-            error={firstnameError}
-            value={firstname}
-            id="firstname"
-            label="FirstName"
-            variant="outlined"
-            onChange={onFirstNameChange}
-          />
-          {firstnameError && <p className="text-red-600">Invalid input.</p>}
-          <TextField
-            error={lastnameError}
-            value={lastname}
-            id="lastname"
-            label="LastName"
-            variant="outlined"
-            onChange={onLastNameChange}
-          />
-          {lastnameError && <p className="text-red-600">Invalid input.</p>}
-          <TextField
-            error={charityidError}
-            value={charityid}
-            id="charityid"
-            label="Charity Id"
-            variant="outlined"
-            onChange={onCharityIdChange}
-          />
-          {charityidError && <p className="text-red-600">Invalid input.</p>}
-          <TextField
-            error={passwordError}
+            error={error}
             value={password}
             id="password"
             label="Password"
             type="password"
             autoComplete="current-password"
-            onChange={onPasswordChange}
+            onChange={onChange}
           />
-          {passwordError && <p className="text-red-600">Invalid input.</p>}
-          <Button type="submit" disabled={submitDisabled} variant="outlined">
-            Sign Up
+          {error && <p className="text-red-600">Invalid login credetials.</p>}
+          <Link href="/forgot-password">Forgot password</Link>
+          <Button
+            type="submit"
+            disabled={email.length === 0 || password.length < 8 ? true : false}
+            variant="outlined"
+          >
+            Sign In
           </Button>
         </form>
         {/*Google oAuth */}
         <Link className="self-center mt-6" href="/sign-up">
-          Sign In Instead
+          Sign Up Instead
         </Link>
       </div>
     </>
   );
 }
 
-export default SignUp;
+export default SignIn;
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,38 +1,137 @@
-import {
-  InputAdornment,
-  FormControl,
-  InputLabel,
-  TextField,
-  OutlinedInput,
-  IconButton,
-  Button,
-} from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import React, { useState, ChangeEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import { Link } from "@mui/material";
-function SignIn() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const [error, setError] = useState(false);
+import { setDoc, doc, serverTimestamp, FieldValue } from "firebase/firestore";
 
-  const { email, password } = formData;
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
+function SignUp() {
+  type FormType = {
+    email: string;
+    password?: string;
+    firstname: string;
+    lastname: string;
+    charityid: string;
+    timestamp?: FieldValue;
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [formData, setFormData] = useState<FormType>({
+    email: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    charityid: "",
+  });
+  const [error, setError] = useState(true);
+  const [emailError, setEmailError] = useState(true);
+  const [firstnameError, setFirstnameError] = useState(true);
+  const [lastnameError, setLastnameError] = useState(true);
+  const [charityidError, setCharityidError] = useState(true);
+  const [passwordError, setPasswordError] = useState(true);
+
+  const { email, password, firstname, lastname, charityid } = formData;
   const navigate = useNavigate();
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setError(false);
+  const isSubmitDisabled = () => {
+    return (
+      emailError ||
+      firstnameError ||
+      lastnameError ||
+      charityidError ||
+      passwordError
+    );
+  };
+  const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmailError(true);
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+    if (isValidEmail(e.target.value)) {
+      setEmailError(false);
+    }
+    setSubmitDisabled(isSubmitDisabled());
   };
+  const onFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFirstnameError(true);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+    if (e.target.value.length > 5) {
+      setFirstnameError(false);
+    }
+    setSubmitDisabled(isSubmitDisabled());
+  };
+  const onLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLastnameError(true);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+    if (e.target.value.length > 5) {
+      setLastnameError(false);
+    }
+    setSubmitDisabled(isSubmitDisabled());
+  };
+  const onCharityIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCharityidError(true);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+    if (e.target.value.length > 5 && Number.isInteger(Number(charityid))) {
+      setCharityidError(false);
+    }
+    setSubmitDisabled(isSubmitDisabled());
+  };
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordError(true);
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+    if (e.target.value.length > 7) {
+      setPasswordError(false);
+    }
+    setSubmitDisabled(isSubmitDisabled());
+  };
+
   function isValidEmail(email: string) {
     return /\S+@\S+\.\S+/.test(email);
   }
-  const onSubmit = () => {
-    if (isValidEmail(email)) {
-      return navigate("/");
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password as string
+      );
+      const user = userCredential.user;
+      if (auth.currentUser) {
+        updateProfile(auth.currentUser, {
+          displayName: firstname,
+        });
+      }
+
+      const formDataCopy: FormType = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+
+      navigate("/");
+    } catch (err) {
+      console.log(err);
     }
-    return setError(true);
   };
   return (
     <>
@@ -42,40 +141,63 @@ function SignIn() {
         </header>
         <form className="flex flex-col gap-3 mx-6" onSubmit={onSubmit}>
           <TextField
-            error={error}
+            error={emailError}
             value={email}
             id="email"
             label="Email"
             variant="outlined"
-            onChange={onChange}
+            onChange={onEmailChange}
           />
+          {emailError && <p className="text-red-600">Invalid input.</p>}
 
           <TextField
-            error={error}
+            error={firstnameError}
+            value={firstname}
+            id="firstname"
+            label="FirstName"
+            variant="outlined"
+            onChange={onFirstNameChange}
+          />
+          {firstnameError && <p className="text-red-600">Invalid input.</p>}
+          <TextField
+            error={lastnameError}
+            value={lastname}
+            id="lastname"
+            label="LastName"
+            variant="outlined"
+            onChange={onLastNameChange}
+          />
+          {lastnameError && <p className="text-red-600">Invalid input.</p>}
+          <TextField
+            error={charityidError}
+            value={charityid}
+            id="charityid"
+            label="Charity Id"
+            variant="outlined"
+            onChange={onCharityIdChange}
+          />
+          {charityidError && <p className="text-red-600">Invalid input.</p>}
+          <TextField
+            error={passwordError}
             value={password}
             id="password"
             label="Password"
             type="password"
             autoComplete="current-password"
-            onChange={onChange}
+            onChange={onPasswordChange}
           />
-          {error && <p className="text-red-600">Invalid login credetials.</p>}
-          <Link href="/forgot-password">Forgot password</Link>
-          <Button
-            type="submit"
-            disabled={email.length === 0 || password.length < 8 ? true : false}
-            variant="outlined"
-          >
-            Sign In
+          {passwordError && <p className="text-red-600">Invalid input.</p>}
+          <Button type="submit" disabled={submitDisabled} variant="outlined">
+            Sign Up
           </Button>
         </form>
         {/*Google oAuth */}
         <Link className="self-center mt-6" href="/sign-in">
-          Sign Up Instead
+          Sign In Instead
         </Link>
       </div>
     </>
   );
 }
 
-export default SignIn;
+export default SignUp;
